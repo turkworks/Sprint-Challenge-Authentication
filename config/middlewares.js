44ccc047c56601479d2ec8c890977b3c1a08ef7a
turkworks <andrewcengiz@gmail.com>
2019-01-25 10:31:40 -0700

    almost finished with mvp

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,27 +1,41 @@
-const jwt = require('jsonwebtoken');
-
-const jwtKey = require('../_secrets/keys').jwtKey;
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const jwtKey = require("../_secrets/keys").jwtKey;
 
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  generateToken
 };
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const secret = jwtKey;
+
+  const options = {
+    expiresIn: "10m"
+  };
+  return jwt.sign(payload, secret, options);
+}
 
 // implementation details
 function authenticate(req, res, next) {
-  const token = req.get('Authorization');
+  const token = req.get("Authorization");
 
   if (token) {
-    jwt.verify(token, jwtKey, (err, decoded) => {
-      if (err) return res.status(401).json(err);
-
-      req.decoded = decoded;
-
-      next();
+    jwt.verify(token, jwtKey, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json(err);
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
     });
   } else {
     return res.status(401).json({
-      error: 'No token provided, must be set on the Authorization Header',
+      error: "No token provided, must be set on the Authorization Header"
     });
   }
 }
--- config/routes.js
+++ config/routes.js
@@ -1,28 +1,66 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const { authenticate } = require('./middlewares');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+
+const server = express();
+knexConfig = require("../knexfile");
+const db = knex(knexConfig.development);
+
+server.use(cors());
+server.use(express.json());
+server.use(helmet());
+
+const { authenticate, generateToken } = require("./middlewares");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
-  // implement user registration
+  const userInfo = req.body;
+  const hash = bcrypt.hashSync(userInfo.password, 14);
+  userInfo.password = hash;
+
+  db("users")
+    .insert(userInfo)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err =>
+      res.status(500).json({ message: "register went wrong, bro" })
+    );
 }
 
 function login(req, res) {
-  // implement user login
+  const cred = req.body;
+
+  db("users")
+    .where({ username: cred.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(cred.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: `welcome ${user.username}`, token });
+      } else {
+        res.status(401).json({ you: "shall not pass, human!" });
+      }
+    })
+    .catch(err => res.status(500).json({ message: "login failed" }));
 }
 
 function getJokes(req, res) {
   axios
-    .get('https://safe-falls-22549.herokuapp.com/random_ten')
+    .get("https://safe-falls-22549.herokuapp.com/random_ten")
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
